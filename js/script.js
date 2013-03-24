d402bbd521743b600f3744a2113a3bad94d2e100
Manik Rathee <manikrathee@gmail.com>
2013-03-24 15:58:32 -0700

    added analytics

--- js/script.js
+++ js/script.js
@@ -6,6 +6,26 @@
  */
 
 //@codekit-prepend "libs/jquery-1.7.1.min.js";
-//@codekit-prepend "libs/ss-standard.js";
-//@codekit-prepend "libs/ss-social.js";
 
+// "libs/ss-standard.js";
+// "libs/ss-social.js";
+
+
+var win   = $(window);
+var doc   = $(document);
+var body  = $('body');
+var $self = $(this);
+
+
+/*  Google Analytics  */
+var googleAnalyticsID = UA-;
+
+var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', googleAnalyticsID]);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
